c677f5616c2f4482f1720718ef4a7c2c89918256
VL4LV <venus.lobo@unitec.edu>
2024-02-16 13:49:43 -0600

    push 1

---
+++ ExamenLab5P2/src/examenlab5p2/Usuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenlab5p2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Usuario {
+    protected String nombre; 
+    protected String apellido; 
+    
+    
+}
